ab60fa58a28b62f17a4eacdfc7910a5ccacc5cc4
Doruk Özer <dorukozer@protonmail.com>
2024-11-22 18:44:11 +0300

    chore: prettier setup
    - installed prettier eslint-prettier and tailwind-prettier
    - configured .prettierrc
    - configured .eslintrc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 const rubik = Rubik({
   weight: ["300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin", "latin-ext"]
+  subsets: ["latin", "latin-ext"],
 });
 
 export const metadata: Metadata = {
