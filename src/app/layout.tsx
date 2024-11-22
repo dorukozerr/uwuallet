ae5cdab41d53a0b1b4178efc7a6d75989459435c
Doruk Özer <dorukozer@protonmail.com>
2024-11-22 19:34:49 +0300

    feat: shadcn init
    - initialized shadcn structure
    - updated tailwind config
    - installed needed dependencies
    - button component added
    - dropdown menu component added
    - input component added
    - configured next-themes
    - updated layout
    - added navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeToggler } from "@/components/theme-toggler";
 import "./globals.css";
 
 const rubik = Rubik({
@@ -13,14 +15,33 @@ export const metadata: Metadata = {
     "Simple expense tracker application, made with nextjs, ts, and mongodb.",
 };
 
-export default function RootLayout({
+const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) => {
   return (
-    <html lang="en">
-      <body className={`antialiased ${rubik.className}`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={rubik.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="mx-auto flex h-full w-full max-w-[1440px] flex-col">
+            <header className="h-18 flex w-full items-center justify-between border-b border-border p-4">
+              <h1 className="text-lg font-bold leading-tight sm:text-2xl">
+                Expense Tracker
+              </h1>
+              <ThemeToggler />
+            </header>
+            <section className="h-full w-full flex-1 p-4">{children}</section>
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <h1>Expense Tracker</h1>;
+  return <></>;
 };
 
 export default Home;
