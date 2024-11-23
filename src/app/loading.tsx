c12fe9f17b93e589052ccff65eacf5cbeaba51e7
Doruk Özer <dorukozer@protonmail.com>
2024-11-23 23:05:11 +0300

    feat: loading state
    - removed react loading icons package

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,11 @@
-import { Puff } from "react-loading-icons";
-
 const Loading = () => (
   <div className="flex h-full w-full items-center justify-center">
-    <Puff className="h-[1.2rem] w-[1.2rem]" />
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="relative">
+        <div className="h-12 w-12 animate-spin rounded-full border-4 border-muted-foreground border-t-primary"></div>
+        <div className="absolute left-1/2 top-1/2 h-3 w-3 -translate-x-1/2 -translate-y-1/2 animate-pulse rounded-full bg-primary"></div>
+      </div>
+    </div>
   </div>
 );
 
