3262074718074de163ca000160a5617c1cfaa9c9
Doruk Özer <dorukozer@protonmail.com>
2024-12-05 02:53:39 +0300

    refactor: info section
    - pie chart data generation logic moved to util function
    - area chart data generation logic moved to util function
    - limits report generation logic moved to util function

--- src/components/dashboard/info-section.tsx
+++ src/components/dashboard/info-section.tsx
@@ -9,7 +9,12 @@ import { getMetrics } from "@/actions/metrics";
 import { useScreenSize } from "@/hooks/useScreenSize";
 import { Transaction } from "@/types";
 import { limitsFormSchema } from "@/lib/schemas";
-import { cn } from "@/lib/utils";
+import {
+  cn,
+  generatePieChartData,
+  generateAreaChartData,
+  generateLimitsReport,
+} from "@/lib/utils";
 import { expenseGroups } from "@/lib/constants";
 import { WarningDialog } from "@/components/dialogs/warning-dialog";
 import { AISummaryDialog } from "@/components/dialogs/ai-summary-dialog";
@@ -84,262 +89,20 @@ export const InfoSection = ({
     return config;
   }, []) satisfies ChartConfig;
 
-  const pieChartData = useMemo(() => {
-    if (metrics) {
-      const chartData: {
-        group: string;
-        totalAmount: number;
-        fill: string;
-      }[] = [];
+  const pieChartData = useMemo(
+    () => generatePieChartData({ metrics, date }),
+    [metrics, date]
+  );
 
-      Object.entries(metrics.chartData.expenses).forEach(([key, value]) => {
-        const expenseDateArr: (string | number)[] = key.split("-");
+  const areaChartData = useMemo(
+    () => generateAreaChartData({ metrics, date }),
+    [metrics, date]
+  );
 
-        expenseDateArr[0] = Number(expenseDateArr[0]) - 1;
-        expenseDateArr[1] = Number(expenseDateArr[1]);
-
-        if (date?.from || date?.to) {
-          if (
-            date?.from &&
-            date.from < new Date(expenseDateArr[1], expenseDateArr[0])
-          ) {
-            if (date?.to) {
-              if (date.to > new Date(expenseDateArr[1], expenseDateArr[0])) {
-                Object.entries(value).map((entry) => {
-                  const matchedData = chartData.find(
-                    (d) => d.group === entry[0]
-                  );
-
-                  if (matchedData) {
-                    matchedData.totalAmount += entry[1];
-                  } else {
-                    chartData.push({
-                      group: entry[0],
-                      totalAmount: entry[1],
-                      fill: `var(--color-${entry[0]})`,
-                    });
-                  }
-                });
-
-                return;
-              } else {
-                return;
-              }
-            }
-            Object.entries(value).map((entry) => {
-              const matchedData = chartData.find((d) => d.group === entry[0]);
-
-              if (matchedData) {
-                matchedData.totalAmount += entry[1];
-              } else {
-                chartData.push({
-                  group: entry[0],
-                  totalAmount: entry[1],
-                  fill: `var(--color-${entry[0]})`,
-                });
-              }
-            });
-
-            return;
-          }
-        } else {
-          Object.entries(value).map((entry) => {
-            const matchedData = chartData.find((d) => d.group === entry[0]);
-
-            if (matchedData) {
-              matchedData.totalAmount += entry[1];
-            } else {
-              chartData.push({
-                group: entry[0],
-                totalAmount: entry[1],
-                fill: `var(--color-${entry[0]})`,
-              });
-            }
-          });
-        }
-      });
-
-      return chartData;
-    } else {
-      return [];
-    }
-  }, [date, metrics]);
-
-  const areaChartData = useMemo(() => {
-    const sortData = (
-      data: {
-        date: string;
-        expenses: number;
-        incomes: number;
-      }[]
-    ) =>
-      data.sort((a, b) => {
-        const [monthA, yearA] = a.date.split("-").map(Number);
-        const [monthB, yearB] = b.date.split("-").map(Number);
-
-        const dateA = new Date(yearA, monthA - 1);
-        const dateB = new Date(yearB, monthB - 1);
-
-        return dateA.getTime() - dateB.getTime();
-      });
-
-    if (metrics) {
-      let chartData: {
-        date: string;
-        expenses: number;
-        incomes: number;
-      }[] = [];
-
-      Object.entries(metrics.chartData.expenses).forEach((expense) => {
-        const dateStr = expense[0];
-        const matchedData = chartData.find((d) => d.date === dateStr);
-
-        const sum = Object.values(expense[1]).reduce(
-          (acc, curr) => acc + curr,
-          0
-        );
-
-        if (matchedData) {
-          matchedData.expenses = sum;
-        } else {
-          chartData.push({
-            date: dateStr,
-            incomes: 0,
-            expenses: sum,
-          });
-        }
-      });
-
-      Object.entries(metrics.chartData.incomes).forEach((income) => {
-        const dateStr = income[0];
-        const matchedData = chartData.find((d) => d.date === dateStr);
-
-        const sum = Object.values(income[1]).reduce(
-          (acc, curr) => acc + curr,
-          0
-        );
-
-        if (matchedData) {
-          matchedData.incomes = sum;
-        } else {
-          chartData.push({
-            date: dateStr,
-            incomes: sum,
-            expenses: 0,
-          });
-        }
-      });
-
-      if (date?.from || date?.to) {
-        const from = date.from;
-        const to = date.to;
-
-        if (from && !to) {
-          chartData = chartData.filter(
-            (data: { date: string }) =>
-              from <
-              new Date(
-                Number(data.date.split("-")[1]),
-                Number(data.date.split("-")[0])
-              )
-          );
-        } else if (from && to) {
-          chartData = chartData.filter(
-            (data: { date: string }) =>
-              from <
-                new Date(
-                  Number(data.date.split("-")[1]),
-                  Number(data.date.split("-")[0])
-                ) &&
-              to >
-                new Date(
-                  Number(data.date.split("-")[1]),
-                  Number(data.date.split("-")[0])
-                )
-          );
-        }
-      }
-
-      return sortData(chartData);
-    }
-  }, [date, metrics]);
-
-  const limitsReport = useMemo(() => {
-    const exceededlimitsData: {
-      date: string;
-      group: string;
-      amount: number;
-      limit: number;
-    }[] = [];
-    let report: {
-      [key: string]: {
-        [key: string]: number;
-      };
-    } = {};
-
-    const sortData = (
-      data: {
-        date: string;
-        group: string;
-        amount: number;
-        limit: number;
-      }[]
-    ) =>
-      data.sort((a, b) => {
-        const [monthA, yearA] = a.date.split("-").map(Number);
-        const [monthB, yearB] = b.date.split("-").map(Number);
-
-        const dateA = new Date(yearA, monthA - 1);
-        const dateB = new Date(yearB, monthB - 1);
-
-        return dateA.getTime() - dateB.getTime();
-      });
-
-    Object.entries(metrics?.chartData?.expenses || {}).forEach((expense) => {
-      Object.entries(expense[1]).forEach((entry) => {
-        const groupName = entry[0] || "";
-
-        report = {
-          ...report,
-          [expense[0]]: report[expense[0]]
-            ? {
-                ...report[expense[0]],
-                [groupName]: report[expense[0]][groupName]
-                  ? report[expense[0]][groupName] + entry[1]
-                  : entry[1],
-              }
-            : {
-                [groupName]: entry[1],
-              },
-        };
-      });
-    });
-
-    Object.entries(report).forEach((entry) => {
-      const date = entry[0];
-
-      Object.entries(entry[1]).forEach((expense) => {
-        const expenseLimit = limits
-          ? limits[expense[0] as keyof Omit<typeof limits, "username">]
-          : 0;
-
-        if (limits && expenseLimit < expense[1] && expenseLimit > 0) {
-          exceededlimitsData.push({
-            date,
-            group: expense[0],
-            amount: expense[1],
-            limit: expenseLimit,
-          });
-        }
-      });
-    });
-
-    if (exceededlimitsData.length > 0) {
-      setWarningDialogState({ open: true });
-    }
-
-    return sortData(exceededlimitsData);
-  }, [metrics, limits]);
+  const limitsReport = useMemo(
+    () => generateLimitsReport({ metrics, limits }),
+    [metrics, limits]
+  );
 
   return (
     <div className="flex h-[1200px] w-full flex-col justify-start gap-4 md:h-[900px] lg:h-[500px]">
